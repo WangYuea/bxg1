7cf134657f0f1716bfb05706d2b1f7e94273852f
7777 <123456789@163.com>
2017-09-19 00:16:38 +0800

    完善登录退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,27 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+$('#logoutBtn').click(function(){
+	$.ajax({
+		type:'post',
+		url:'/api/logout',
+		dataType:'json',
+		success:function(data){
+			if(data.code==200){
+				location.href='/main/login';
+			}
+		}
+	});
+});
+	//验证用户是否登录
+var flag= $.cookie('PHPSESSID');
+	//console.log(flag);
+if(!flag){
+	location.href='/main/login';
+}
+	var loginInfo=$.cookie('loginInfo');
+	//console.log(loginInfo);
+	loginInfo=loginInfo&&JSON.parse(loginInfo);
+	$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+	$('.aside .profile h4').html(loginInfo.tc_name);
