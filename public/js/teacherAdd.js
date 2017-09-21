520aedbd983ffe115dd3fd0c7019078220385a16
7777 <123456789@163.com>
2017-09-21 21:53:04 +0800

    完成添加和编辑讲师的提交功能

--- public/js/teacherAdd.js
+++ public/js/teacherAdd.js
@@ -16,11 +16,30 @@ define(['jquery','template','util'],function($,template,util){
                 data.result.operate='编辑讲师';
                 var html=template('teacherTpl',data.result);
                 $('#teacherInfo').html(html);
+                submitForm('/api/teacher/update');
             }
         })
     }else{
       //添加操作
         var html=template('teacherTpl',{operate:'添加讲师'});
         $('#teacherInfo').html(html);
+        submitForm('/api/teacher/add');
+    }
+    //提交表单
+    function submitForm(url){
+        $('#teacherBtn').click(function(){
+            $.ajax({
+                type:'post',
+                url:url,
+                data:$('#teacherForm').serialize(),
+                dataType:'json',
+                success:function(data){
+                    console.log(data);
+                    if(data.code==200){
+                        location.href='/teacher/teacher_list';
+                    }
+                }
+            });
+        });
     }
 });
