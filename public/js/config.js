47896044dca9fbe7fc335d2e5eb36b6cab8bc069
7777 <123456789@163.com>
2017-09-22 22:49:12 +0800

    完成头像上传操作

--- public/js/config.js
+++ public/js/config.js
@@ -12,6 +12,7 @@ require.config({
         language: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
         validate:'validate/jquery-validate.min',
         form:'jquery-form/jquery.form',
+        uploadify:'uploadify/jquery.uploadify.min',
         util: '../js/util',
         common: '../js/common',
         login: '../js/login',
@@ -27,6 +28,9 @@ require.config({
             deps:['jquery','datepicker']
         },
         validate:{
+            deps:['jquery']
+        },
+        uploadify:{
             deps:['jquery']
         }
     }
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/9/22.
  */
-define(['jquery','template'],function($,template){
+define(['jquery','template','uploadify'],function($,template){
     $.ajax({
         type:'get',
         url:'/api/teacher/profile',
@@ -10,6 +10,22 @@ define(['jquery','template'],function($,template){
             //console.log(data);
             var html=template('settingsTpl',data.result);
             $('#settingsInfo').html(html);
+            //处理头像上传
+            $('#upfile').uploadify({
+                swf:'/public/assets/uploadify/uploadify.swf',
+                uploader:'/api//uploader/avatar',
+                fileObjName:'tc_avatar',
+                buttonText:'',
+                width:120,
+                height:120,
+                itemTemplate:'<span></span>',
+                onUploadSuccess:function(a,b){
+
+                    var obj=JSON.parse(b);
+                    console.log(b);
+                    $('.preview img').attr('src',obj.result.path);
+                }
+            });
         }
     })
 });
