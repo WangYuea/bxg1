04e9356dc191c3909e2371bf84359195f1b81cdd
7777 <123456789@163.com>
2017-09-24 17:36:27 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/9/22.
  */
-define(['jquery','template','ckeditor','uploadify','region','datepicker','language'],function($,template,CKEDITOR){
+define(['jquery','template','ckeditor','uploadify','region','datepicker','language','validate','form'],function($,template,CKEDITOR){
     $.ajax({
         type:'get',
         url:'/api/teacher/profile',
@@ -21,7 +21,6 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
                 height:120,
                 itemTemplate:'<span></span>',
                 onUploadSuccess:function(a,b){
-
                     var obj=JSON.parse(b);
                     console.log(b);
                     $('.preview img').attr('src',obj.result.path);
@@ -30,6 +29,37 @@ define(['jquery','template','ckeditor','uploadify','region','datepicker','langua
             //省市县三级联动
             $('#pcd').region({
                url:'/public/assets/jquery-region/region.json'
+            });
+            //处理表单提交
+            $('#settingsForm').validate({
+                sendForm:false,
+                valid: function () {
+                    //拼接籍贯
+                    var p=$("#p").find('option:selected').text();
+                    var c=$("#d").find('option:selected').text();
+                    var d=$("#d").find('option:selected').text();
+                    var hometown=p+'|'+c+'|'+d;
+                    //更新文本内容
+                    for(var instance in CKEDITOR.instances){
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    //console.log(p);
+                    //提交表单
+                    $(this).ajaxSubmit({
+                       type:'post',
+                        url:'/api/teacher/modify',
+                        data:{
+                            tc_hometown:hometown
+                        },
+                        dataType:'json',
+                        success:function(data){
+                            //console.log(data);
+                            if(data.code==200){
+                                location.reload();
+                            }
+                        }
+                    });
+                }
             });
         }
     })
