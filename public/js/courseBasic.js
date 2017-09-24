97868ce5a397564b26aa1809cf4eb147745469be
7777 <123456789@163.com>
2017-09-24 22:26:59 +0800

    完成课程基本信息填充

--- public/js/courseBasic.js
+++ public/js/courseBasic.js
@@ -23,7 +23,25 @@ define(['jquery', 'template', 'util'], function ($, template, util) {
             }
             var html=template('basicTpl',data.result);
             $('#basicInfo').html(html);
-
+            //处理二级分类的下拉联动操作
+            $('#firstType').change(function(){
+                //获取一级分类id
+                var pid=$(this).val();
+                //console.log(pid);
+                //根据一级分类的id查询所有的二级分类数据
+                $.ajax({
+                    type:'get',
+                    url:'/api/category/child',
+                    data:{cg_id:pid},
+                    dataType:'json',
+                    success:function(data){
+                        //console.log(data);
+                        var tpl=' <select name="" class="form-control input-sm"> <option value="">请选择二级分类</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option> {{/each}}</select>';
+                        var html= template.render(tpl,{list:data.result});
+                        $("#secondType").html(html);
+                    }
+                })
+            })
         }
     })
 });
